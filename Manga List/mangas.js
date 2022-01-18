8998e45076f42ea43e3a3b550edb4be2900c5ecd
Prajwol Shrestha <70046321+Prajwol-Shrestha@users.noreply.github.com>
2022-01-18 19:15:20 +0545

    Manga List

---
+++ Manga List/mangas.js
@@ -0,0 +1,108 @@
+// Dynamic Ratings Left To Built
+
+
+mangas = [
+	{
+		id: 0,
+		name: 'Above Ten Thousand People',
+		img: './images/aboveTenThousandPeople.jpg',
+		tags: 'Cultivation, Comedy, Reincarnation',
+		translation: 'Excellent',
+		rating: 3,
+	},
+
+	{
+		id: 1,
+		name: 'Martial Peak',
+		img: './images/martialPeak.jpg',
+		tags: 'Cultivation, Martial Arts',
+		translation: 'Excellent',
+		rating: 3,
+	},
+
+	{
+		id: 2,
+		name: 'Apotheosis',
+		img: './images/apotheosis.jpg',
+		tags: 'Cultivation, Martial Arts',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+	{
+		id: 3,
+		name: 'Demon Magic Emperor',
+		img: './images/magicEmperor.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 4,
+		name: 'Tales Of Demons And Gods',
+		img: './images/talesOfDemonsAndGods.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 5,
+		name: 'The Constellation That Returned From Hell',
+		img: './images/theConstellationThatReturnedFromHell.jpg',
+		tags: 'Cultivation, Martial Arts ',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 6,
+		name: 'Wandering Warrior of Wudang',
+		img: './images/wanderingWarriorOfWudang.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 7,
+		name: "Player That Can't Level Up",
+		img: './images/playerThatCannotLevelUp.jpg',
+		tags: '',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 8,
+		name: "Return Of Mount Hua Sect",
+		img: './images/returnOfMountHuaSect.jpg',
+		tags: 'Cultivation, Martial Arts, Reincarnation',
+		translation: 'Excellent',
+		rating: 3,
+
+	},
+
+
+	{
+		id: 9,
+		name: "Cultivator Against Hero Society",
+		img: './images/cultivatorAgainstHeroSociety.jpg',
+		tags: 'Cultivation, Isekai',
+		translation: 'Excellent',
+		rating: 3,
+	},
+
+]
