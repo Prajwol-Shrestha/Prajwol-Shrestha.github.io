8360eab4dc498a8dc90e419e5c702e8bcce10af5
Prajwol Shrestha <70046321+Prajwol-Shrestha@users.noreply.github.com>
2022-02-15 20:06:17 +0545

    JS projects

---
+++ JS Projects/FAQ page/FAQs.js
@@ -0,0 +1,23 @@
+const obj = [
+	{
+		id: 0,
+		qn: 'FAQ question one?',
+		ans: 'FAQ answer one',
+	},
+
+	{
+		id: 1,
+		qn: 'Who are you?',
+		ans: 'I am Prajwol Shrestha',
+	},
+	{
+		id: 2,
+		qn: 'FAQ question two?',
+		ans: 'FAQ answer two',
+	},
+	{
+		id: 3,
+		qn: 'FAQ question three?',
+		ans: 'FAQ question three',
+	}
+];
